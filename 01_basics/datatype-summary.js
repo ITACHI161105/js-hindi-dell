495a1cae47975cff64efe506e7bc83cae5c91939
utkarsh <133217805+ITACHI161105@users.noreply.github.com>
2025-04-26 11:01:56 +0000

    datatype-summary.js

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -4,6 +4,18 @@
 
 // JavaScript is not defined language like like typescript
 
+ 
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
 
 // Reference (Non primitive)
 
