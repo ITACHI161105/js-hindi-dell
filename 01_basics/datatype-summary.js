b7b47c411b7e947e24ea899e75b9baf1fa79fb87
utkarsh <133217805+ITACHI161105@users.noreply.github.com>
2025-04-26 10:28:25 +0000

    datatype-summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,10 @@
+ // Primitive 
+  
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// JavaScript is not defined language like like typescript
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
